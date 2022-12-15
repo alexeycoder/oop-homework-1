053c06cb8b39fcf68f4e4fe4ae968a97c40a0b04
Alexey Kuznetsov <leoxander@yandex.ru>
2022-12-15 14:56:47 +0300

    Minor update

--- src/main/java/edu/oop/schooladmin/consoleclient/App.java
+++ src/main/java/edu/oop/schooladmin/consoleclient/App.java
@@ -1,13 +1,17 @@
 package edu.oop.schooladmin.consoleclient;
 
+import java.nio.charset.Charset;
+import java.nio.charset.spi.CharsetProvider;
 import java.util.Scanner;
 
 import edu.oop.schooladmin.consoleclient.controllers.MainController;
 import edu.oop.schooladmin.model.implementations.testdb.TestDbProvider;
+import edu.oop.schooladmin.model.interfaces.DataProvider;
 
 public class App {
 	public static void main(String[] args) {
-		TestDbProvider dataProvider = new TestDbProvider();
+
+		DataProvider dataProvider = new TestDbProvider();
 		Scanner consoleInputScanner = new Scanner(System.in);
 		MainController mainController = new MainController(dataProvider, consoleInputScanner);
 
